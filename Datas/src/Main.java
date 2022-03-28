f86dc3aad4669f46621687e2fa328ffa314201cf
RafaelRibeiroEstrela <74848416+RafaelRibeiroEstrela@users.noreply.github.com>
2022-03-28 11:48:59 -0400

    update

--- Datas/src/Main.java
+++ Datas/src/Main.java
@@ -2,11 +2,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		
 		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
@@ -19,10 +21,14 @@ public class Main {
 		LocalDateTime dataHora = LocalDateTime.parse("10/10/1990 10:00:00", dateTimeFormat);
 		
 		
+		long diferencaEmAnos = ChronoUnit.YEARS.between(data, LocalDate.now());
+		
 		
 		System.out.println(data);
 		System.out.println(hora);
 		System.out.println(dataHora);
+		
+		System.out.println(diferencaEmAnos);
 		
 
 	}
